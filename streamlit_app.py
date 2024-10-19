4c9f402468225c0a41bbf525802f51fee358e3bf
amitsingh115 <146443610+amitsingh115@users.noreply.github.com>
2024-10-19 19:37:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import panda as pd
 
 st.title('Smart Grid Management System')
 
 st.info('This app builds smart grid management systems')
+
+df = pd.read_csv('https://raw.githubusercontent.com/amitsingh115/PowerGrid-Pioneers/refs/heads/master/Power%20Stations.csv')
+df
