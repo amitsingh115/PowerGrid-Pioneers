b8901d819e697ee7843397b2b8918874ff02c4f6
amitsingh115 <146443610+amitsingh115@users.noreply.github.com>
2024-10-19 19:00:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('Smart Grid Management System')
 
-st.write('Hello world!')
+st.info(' Smart Grid Management System (SGMS) to improve green energy integration into existing power grids. 
+The SGMS address key challenges - implementing real-time energy management algorithms to handle the variability of renewable sources like solar and wind, ensuring a stable power supply. 
+')
