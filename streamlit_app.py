8ed978bd4c3e94b4c715d663caf48117dc982da1
amitsingh115 <146443610+amitsingh115@users.noreply.github.com>
2024-10-20 01:28:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ with st.sidebar:
 with st.expander('Total Renewable energy production'):
  st.write('**Total renewable energy production**')
  df = pd.read_csv('https://raw.githubusercontent.com/amitsingh115/PowerGrid-Pioneers/refs/heads/master/.streamlit/total%20renewable%20energy%20power%20generation.csv')
- df = pf.read_csv 
+ df = pf.read_csv('https://raw.githubusercontent.com/amitsingh115/PowerGrid-Pioneers/refs/heads/master/.streamlit/nuclear%20power%20plant%20data.csv')
  df
 
 with st.expander('Total Renewable energy production line chart'):
